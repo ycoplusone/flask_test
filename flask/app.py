c3a0566d999c62dfd25c7f25b5ba079168e2f71a
fish <ycoplusone@naver.com>
2022-10-25 18:01:48 +0900

    20221025 finish

--- flask/app.py
+++ flask/app.py
@@ -6,8 +6,15 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route('/' , methods=('GET','POST'))
 def index():
-    return 'app.py index is good'
-            
+    if request.method =='POST':        
+        print('forget you')
+        user = request.form.get('user')
+        data = {'level': 60, 'point': 360, 'exp': 45000}
+        return render_template('index.html', user=user, data=data)
+    elif request.method == 'GET':
+        user = 'ㅎㅎㅎ'
+        data = {'level': 60, 'point': 360, 'exp': 45000}
+        return render_template('index.html', user=user, data=data)     
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000 , debug=True )
