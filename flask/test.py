cb366ea47da2da770bb0c79ddd6af5f17ebdc68d
fish <ycoplusone@naver.com>
2022-10-25 15:14:49 +0900

    왜 이러는거야? 깃은 정말 어려워

---
+++ flask/test.py
@@ -0,0 +1,16 @@
+'''
+test.py
+'''
+from flask import Flask , render_template , request
+
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/' , methods=('GET','POST'))
+def index():
+    return 'test.py index is good'
+            
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000 , debug=True )
